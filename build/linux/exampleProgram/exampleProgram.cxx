6ed3446f1c218b57ecf0b50484fcc3683f3fd60c
Mark Millard <mmillard@alticast.com>
2018-11-29 13:25:42 -0700

    Adding additional MleEventDispatcher tests.

--- build/linux/exampleProgram/exampleProgram.cxx
+++ build/linux/exampleProgram/exampleProgram.cxx
@@ -68,7 +68,7 @@ int eventHndlr(MleEvent event,void *callData,void *clientData)
     char *clientStr = (char *)clientData;
     char *callStr = (char *)callData;
 
-    fprintf(stdout,"Received event %d: processing data %s, %s\n",event,clientStr,callStr);
+    fprintf(stdout,"Received event %ld: processing data %s, %s\n",event,clientStr,callStr);
 
     return(0);
 }
--- build/linux/libmleventtest/testMleEventDispatcher.cxx
+++ build/linux/libmleventtest/testMleEventDispatcher.cxx
@@ -38,10 +38,102 @@
 #include "mle/mlMalloc.h"
 #include "mle/MleEventDispatcher.h"
 
+enum {
+    EVENT_ONE = 1,
+    EVENT_TWO = 2,
+    EVENT_THREE = 3,
+    EVENT_FOUR = 4,
+    EVENT_FIVE = 5
+};
+
+// Callback event handler.
+int eventHndlr(MleEvent event,void *callData,void *clientData)
+{
+    // Declare local variables.
+    char *clientStr = (char *)clientData;
+    char *callStr = (char *)callData;
+
+    fprintf(stdout,"Received event %ld: processing data %s, %s\n",event,clientStr,callStr);
+
+    return(0);
+}
+
 TEST(MleEventDispatcherTest, DefaultConstructor) {
     // This test is named "DefaultConstructor", and belongs to the "MleEventDispatcherTest"
     // test case.
 
     MleEventDispatcher *eventMgr = new MleEventDispatcher();
     EXPECT_TRUE(eventMgr != NULL);
+}
+
+TEST(MleEventDispatcherTest, Destructor) {
+    // This test is named "Destructor", and belongs to the "MleEventDispatcherTest"
+    // test case.
+
+    MleEventDispatcher *eventMgr = new MleEventDispatcher();
+    EXPECT_TRUE(eventMgr != NULL);
+
+    delete eventMgr;
+}
+
+TEST(MleEventDispatcherTest, InstallEvent) {
+    // This test is named "InstallEvent", and belongs to the "MleEventDispatcherTest"
+    // test case.
+
+    MleEventDispatcher *eventMgr = new MleEventDispatcher();
+    EXPECT_TRUE(eventMgr != NULL);
+
+    // Install an event.
+    MleCallbackId cbId[5];
+    for (int i = 0; i < 5; i++) cbId[i] = 0;
+    cbId[0] = eventMgr->installEventCB(EVENT_ONE,eventHndlr,(void *)"test");
+    EXPECT_TRUE(cbId[0] != 0);
+
+    delete eventMgr;
+}
+
+TEST(MleEventDispatcherTest, UninstallEvent) {
+    // This test is named "UninstallEvent", and belongs to the "MleEventDispatcherTest"
+    // test case.
+
+    MleEventDispatcher *eventMgr = new MleEventDispatcher();
+    EXPECT_TRUE(eventMgr != NULL);
+
+    // Install an event.
+    MleCallbackId cbId[5];
+    for (int i = 0; i < 5; i++) cbId[i] = 0;
+    cbId[0] = eventMgr->installEventCB(EVENT_ONE,eventHndlr,(void *)"test");
+    EXPECT_TRUE(cbId[0] != 0);
+
+    // Remove event.
+    MlBoolean status = eventMgr->uninstallEvent(EVENT_ONE);
+    EXPECT_TRUE(status);
+
+    delete eventMgr;
+}
+
+TEST(MleEventDispatcherTest, DispatchEvent) {
+    // This test is named "DispatchEvent", and belongs to the "MleEventDispatcherTest"
+    // test case.
+
+    MleEventDispatcher *eventMgr = new MleEventDispatcher();
+    EXPECT_TRUE(eventMgr != NULL);
+
+    // Install an event with callback used for dispatching.
+    MleCallbackId cbId[5];
+    for (int i = 0; i < 5; i++) cbId[i] = 0;
+    cbId[0] = eventMgr->installEventCB(EVENT_ONE,eventHndlr,(void *)"test");
+    EXPECT_TRUE(cbId[0] != 0);
+
+    // Test for successful dispatch.
+    int cbRetValue = eventMgr->dispatchEvent(EVENT_ONE,(void *)"Unit test.");
+    EXPECT_TRUE(cbRetValue == 0);
+
+    // Remove event and test for unsuccessful dispatch.
+    MlBoolean status = eventMgr->uninstallEvent(EVENT_ONE);
+    EXPECT_TRUE(status);
+    cbRetValue = eventMgr->dispatchEvent(EVENT_ONE,(void *)"Unit test.");
+    EXPECT_TRUE(cbRetValue != 0);
+
+    delete eventMgr;
 }
